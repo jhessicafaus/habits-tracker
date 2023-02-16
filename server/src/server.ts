5eace8ce7137c131963f0a5c1d2bbb5e9cf2230d
jfauss <92649040+jhessicafaus@users.noreply.github.com>
2023-02-16 09:53:30 -0400

    First commit

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
+import { notificationRoutes } from './lib/notifications-routes'
+
+const app = Fastify()
+
+app.register(cors, {
+    origin: true,
+})
+app.register(appRoutes)
+app.register(notificationRoutes)
+
+app.listen({
+    port: 3333,
+    host: '0.0.0.0',
+}).then((url) => {
+    console.log(`HTTP Server running on ${url}`)
+})
